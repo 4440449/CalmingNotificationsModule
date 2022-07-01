3d3ddf5ca9c9a030ca633b52a06a53c3b7e0a018
Max <4440449@gmail.com>
2022-07-01 19:23:39 +0300

    fixed coreDataStack init from Bundle.module

--- Sources/CalmingNotificationsModule/Data/Infrastructure/CoreData/CoreDataStack_CN.swift
+++ Sources/CalmingNotificationsModule/Data/Infrastructure/CoreData/CoreDataStack_CN.swift
@@ -12,40 +12,46 @@ import CoreData
 final class CoreDataStack_CN {
     
     // MARK: - Static
-
+    
     static var shared = CoreDataStack_CN()
     
     
     // MARK: - Init
     
     private init() {}
-  
+    
     
     // MARK: - Core Data stack
-
-    lazy var persistentContainer: NSPersistentCloudKitContainer = {
-          let container = NSPersistentCloudKitContainer(name: "CalmingNotifications")
-          container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-              if let error = error as NSError? {
-                  fatalError("Unresolved error \(error), \(error.userInfo)")
-              }
-          })
-          return container
-      }()
-
     
-      // MARK: - Core Data Saving support
-
-      func saveContext () {
-          let context = persistentContainer.viewContext
-          if context.hasChanges {
-              do {
-                  try context.save()
-              } catch {
-                  let nserror = error as NSError
-                  fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-              }
-          }
-      }
+    lazy var persistentContainer: NSPersistentCloudKitContainer = {
+        if let objectModelURL = Bundle.module.url(forResource: "CalmingNotifications", withExtension: "momd"),
+           let mom = NSManagedObjectModel(contentsOf: objectModelURL) {
+            let container = NSPersistentCloudKitContainer(name: "CalmingNotifications",
+                                                          managedObjectModel: mom)
+            container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+                if let error = error as NSError? {
+                    fatalError("Unresolved error \(error), \(error.userInfo)")
+                }
+            })
+            return container
+        } else {
+            fatalError("Unresolved error")
+        }
+    }()
+    
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
     
 }
